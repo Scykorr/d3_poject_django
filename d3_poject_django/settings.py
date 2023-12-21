c0796ce4e99074e99f14f2342ebe4d0cebc194a9
Tony <fedosov-toxa@mail.ru>
2023-12-21 20:18:32 +0300

    first email done

--- d3_poject_django/settings.py
+++ d3_poject_django/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'fedos.py'
-EMAIL_HOST_PASSWORD = 'bzxvzysphqyhkjkz'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
