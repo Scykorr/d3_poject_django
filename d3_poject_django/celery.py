a140996c24bf021f10d2780bfe92c1c12ca6d0c9
Tony <fedosov-toxa@mail.ru>
2024-01-08 16:45:15 +0300

    celery fix v1

--- d3_poject_django/celery.py
+++ d3_poject_django/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'd3_project_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'd3_poject_django.settings')
 
-app = Celery('d3_project_django')
+app = Celery('d3_poject_django')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
